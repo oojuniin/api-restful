f0c762c0358dd53ff4dc78499053113ccbc85694
Junior <juniiorliimatt@gmail.com>
2021-01-14 19:09:34 -0300

    api ok

--- src/main/java/com/api/restful/product/ProductController.java
+++ src/main/java/com/api/restful/product/ProductController.java
@@ -1,13 +1,75 @@
 package com.api.restful.product;
 
-import org.springframework.stereotype.Controller;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class ProductController {
+import lombok.extern.slf4j.Slf4j;
 
-	@GetMapping("/")
-	public String list() {
-		return "/restful/src/main/resources/templates/product.html";
+@RestController
+@RequestMapping("/api/v1/products")
+@Slf4j
+public class ProductAPI implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private ProductService service;
+
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		return ResponseEntity.ok(service.findAll());
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Optional<Product> stock = service.findById(id);
+		
+		if (stock.isEmpty()) {
+			log.error("ID " + id + " is not existed.");
+			ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.ok(stock.get());
+	}
+
+	@PostMapping
+	public ResponseEntity<Product> create(@RequestBody Product product) {
+		return ResponseEntity.ok(service.save(product));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+
+		if (service.findById(id).isEmpty()) {
+			log.error("ID " + id + " is not existed.");
+			ResponseEntity.badRequest().build();
+		}
+
+		return ResponseEntity.ok(service.save(product));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Product> delete(@PathVariable Long id) {
+		if (service.findById(id).isEmpty()) {
+			log.error("ID " + id + " is not existed.");
+			ResponseEntity.notFound().build();
+		}
+
+		service.deleteById(id);
+
+		return ResponseEntity.ok().build();
+	}
+
 }
