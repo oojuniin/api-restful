1db11df00746f5359dc2f9aca9f855c712eb2f9c
Junior <juniiorliimatt@gmail.com>
2021-01-14 12:31:04 -0300

    ProductService created

---
+++ src/main/java/com/api/restful/product/ProductService.java
@@ -0,0 +1,38 @@
+package com.api.restful.product;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	@Transactional
+	public Optional<Product> findById(Long id){
+		return repository.findById(id);
+	}
+	
+	@Transactional
+	public Product save(Product stock) {
+		return repository.save(stock);
+	}
+	
+	@Transactional
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
+}
