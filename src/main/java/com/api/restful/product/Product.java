0ac9ca4f6d7f4c4c4e158f6c655314ca197cf3eb
Junior <juniiorliimatt@gmail.com>
2021-01-14 11:37:17 -0300

    Project configured

---
+++ src/main/java/com/api/restful/product/Product.java
@@ -0,0 +1,35 @@
+package com.api.restful.product;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Product implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String description;
+	private BigDecimal price;
+	
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	private Date updatedAt;
+}
